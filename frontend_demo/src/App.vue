364376fa149041f031ca15170fe32918fdb76cc4
Jeffrey Lu <146838940+JeffreyLuW@users.noreply.github.com>
2023-12-29 14:14:30 +0800

    add_vue_frontend

---
+++ frontend_demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="player in players" :key="player.id">
+        {{ player.name }} - PPG: {{ player.ppg }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import PlayerService from '@/services/PlayerService.js';
+
+export default {
+  data() {
+    return {
+      players: []
+    };
+  },
+  created() {
+    PlayerService.getAllPlayers()
+      .then(response => {
+        this.players = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+</script>
+
